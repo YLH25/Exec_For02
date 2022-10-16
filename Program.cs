72b2e35ccac94148d226a99826a0b4402e4fda83
Yang <Yang@YANG-PC>
2022-10-16 19:14:18 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //等腰星形三角形
+            for (int i = 3; i > 0; i--)
+            {
+                string value = new string(' ', i - 1) + new string('*', 3 - i) + "*" + new string('*', 3 - i) + new string(' ', i - 1);
+                Console.WriteLine(value);
+            }
+            ////等腰星形三角形2
+            //for (int i = 3; i > 0; i--)
+            //{
+            //    for (int j = i; j >= 2; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int j = i; j < 3; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.Write("*");
+            //    for (int j = i; j < 3; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    for (int j = i; j >= 2; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
